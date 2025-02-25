03d50d4cf2b1f3aecad04d017cb22c094ccb2c54
amerpolautocentrum <a.tomaszewski@amer-pol.com>
2025-02-25 18:47:05 +0100

    Add files via upload

--- server.js
+++ server.js
@@ -44,7 +44,7 @@ app.get('/api/proxy', async (req, res) => {
     try {
         const response = await fetchWithRetry(url, {
             headers: {
-                "Authorization": "Bearer TWÓJ_NOWY_TOKEN", // Wstaw nowy token tutaj
+                "Authorization": "Bearer eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX25hbWUiOiIxMDg5MTM2NDIiLCJzY29wZSI6WyJhbGxlZ3JvOmFwaTpzYWxlOm9mZmVyczpyZWFkIl0sImFsbGVncm9fYXBpIjp0cnVlLCJpc3MiOiJodHRwczovL2FsbGVncm8ucGwiLCJleHAiOjE3NDA1NDgwMDksImp0aSI6ImIxOTNjNDQ4LTI5ZTYtNDFkNi1iZGZlLTQ5ZjExZjkzZWIzNSIsImNsaWVudF9pZCI6IjRhNjhkMDk0ZDljMjQ3NTRhNzBlNWY4MWVlNWIxMjQxIn0.123Q0wwdP011vODzvHlD2PiLzOSH4iWByqDISKkvZGqK0kHoARI2L5JElJ0J8XBMPPmHP1Ye-uzE-O2yQECiruFQLh3B-Mac6mrSm6G9kLuJcBo5-Ozvo6a0fsl9OPOlIiQI7_Y51vHoHwlR6ermK4HFFVn7XlIt_ZEu2EdOPIYOrdAKDYBlb__zXUhC8uTXPYdt924SJ_62S4gAjFq3-bAyUCgRdvWNuLa8wHLQfMVXiC3nfRK0rcwO8O5DSZ57LqeHMIR6YaSLVclUQIVxyqLsBxNI0gQ_yX64c3d0DrrfDiDPS8hqeL22ySi7B8hA_1weMsDABeRNdD2l1_2eNg",
                 "Accept": "application/vnd.allegro.public.v1+json",
                 "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64)"
             }
